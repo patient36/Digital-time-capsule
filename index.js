b09b10eac1661d170281146404e075ffa685d9a7
patient36 <topfateson@gmail.com>
2025-02-05 03:30:17 +0200

    Fix: removed a bug from index.js

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 import express from "express"
-import mongoose from "mongoose"
 import dotenv from "dotenv"
-import router from "./routes/message.js"
 import cors from "cors"
+import connectDB from "./config/db.js"
+import { notFound, errorHandler } from "./middlewares/error.js"
 
 dotenv.config()
+connectDB()
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({ origin: process.env.CLIENT }))
-app.use('/api', router)
+app.use(notFound)
+app.use(errorHandler)
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.error("Could not connect to MongoDB", err))
 
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -7,12 +9,19 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
         required: true
     }
 }, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
 
 export const User = mongoose.model("User", userSchema);
