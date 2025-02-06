47a4e52c1a9f70c6b5035ec371434e814969f1bc
patient36 <topfateson@gmail.com>
2025-02-07 01:19:18 +0200

    Feat: defined a model for attachments as files

--- models/attachment.js
+++ models/attachment.js
@@ -1,6 +1,23 @@
 import mongoose from "mongoose";
 
-const attachmentSchema = new mongoose.Schema({})
+const attachmentSchema = new mongoose.Schema({
+    filename: { type: String, required: true },
+    contentType: { type: String, required: true },
+    length: { type: Number, required: true },
+    chunkSize: { type: Number, required: true },
+}, { timestamps: true })
 
+const attachmentChunksSchema = mongoose.Schema({
+    files_id: {
+        type: mongoose.Schema.Types.Objectid,
+        required: true,
+        ref: "Attachment"
+    },
+    n: { type: Number, required: true },
+    data: { type: Buffer, required: true },
+}, { timestamps: true })
 
-export const Attachment = mongoose.model("Attachment", attachmentSchema);
+const Attachment = mongoose.model("Attachment", attachmentSchema);
+const AttachmentChunk = mongoose.model("AttachmentChunk", attachmentChunksSchema)
+
+export { Attachment, AttachmentChunk }
